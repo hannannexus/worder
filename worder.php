6da64fbf893ff0f274c21109f7b9fe6a742ae10b
Hannan <hannannexus@gmail.com>
2023-01-31 12:40:25 +0600

    Create worder.php
    create a new instance for WC_Order Class

---
+++ worder.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Worder
+ * Plugin URI: #
+ * Description: Display woocommerce order details
+ * Author: Hannan
+ * Author URI: #
+ * Version: 1.0
+ * 
+ */
+ 
+ 
+ $orders = new WC_Order();
+ 
+ var_dump($ooders);
